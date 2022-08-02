e99002bd8cd47f1e1cf4db818907cd7d7602366f
Justin Whitaker <justinwhitaker@protonmail.com>
2022-08-02 13:46:54 -0400

    demo

--- demo.js
+++ demo.js
@@ -1,9 +1,23 @@
 const authenticate = require('./auth.js');
-authenticate('john', 'hunter2', 'local');
-authenticate('john', 'hunter3', 'local');
-authenticate('john', 'hunter2', 'saml');
-authenticate('john', 'hunter3', 'saml');
+console.log(authenticate('john', 'hunter2', 'local'));
+console.log(authenticate('john', 'hunter3', 'local'));
+console.log(authenticate('john', 'hunter2', 'saml'));
+console.log(authenticate('john', 'hunter3', 'saml'));
+// failing to await promise
 let authenticated = authenticate('john', 'hunter3', 'saml');
 if(authenticated){
     console.log('Access granted')
 }
+else {
+    console.log('Access denied')
+}
+// awaiting promise
+(async () => {
+    authenticated = await authenticate('john', 'hunter3', 'saml');
+    if(authenticated){
+        console.log('Access granted')
+    }
+    else {
+        console.log('Access denied')
+    }
+})();
